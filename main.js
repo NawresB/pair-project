97a9e1326462609ccf8e2caf63b37d2cf069bd7b
NawresB~ <nawresbenali33@gmail.com>
2023-01-23 16:43:24 +0100

    hii

---
+++ main.js
@@ -0,0 +1,151 @@
+const data=[{
+    src:"/image-project/0.png",
+    title:"white t-shirt ",
+    price:25,
+},{ src:"/image-project/1.jfif",
+title:"high heels",
+price:42,
+
+},{ src:"/image-project/2.jfif",
+title:"shoes",
+price:30,
+
+},{ src:"/image-project/3.jfif",
+title:"channel sunglasses",
+price:150,
+
+},{ src:"/image-project/4.jfif",
+title:"gris t-shirt",
+price:24,
+
+},{ src:"/image-project/5.jfif",
+title:" Black Men's shoes",
+price:35,
+
+},
+{ src:"/image-project/6.jfif",
+title:"black t-shirt",
+price:22,
+
+},
+{ src:"/image-project/7.jfif",
+title:"men's sunglasses",
+price:80,
+
+},
+{ src:"/image-project/8.jfif",
+title:"Black cap",
+price:15,
+
+},
+{ src:"/image-project/9.png",
+title:"white t-shirt",
+price:20,
+
+},
+{ src:"/image-project/10.jfif",
+title:"metallic sunglasses",
+price:95,
+
+},
+{ src:"/image-project/11.jfif",
+title:"nike shoes",
+price:250,
+
+},
+{ src:"/image-project/12.jfif",
+title:"pink sunglasses",
+price:140,
+
+},
+{ src:"/image-project/13.jfif",
+title:"black and modern sunglasses",
+price:135,
+
+},
+{ src:"/image-project/14.jfif",
+title:"glowing cap",
+price:24,
+
+},
+{ src:"/image-project/15.jfif",
+title:"shoes for classy women",
+price:45,
+
+},
+{ src:"/image-project/16.jfif",
+title:"T-shirt for men ",
+price:32,
+
+},{ src:"/image-project/17.jfif",
+title:"simple dress ",
+price:70,
+
+},{ src:"/image-project/18.jfif",
+title:"white dress",
+price:95,
+
+},{ src:"/image-project/19.jfif",
+title:"men's sunglasses",
+price:100,
+
+},{ src:"/image-project/20.jfif",
+title:"white high heels",
+price:115,
+
+},{ src:"/image-project/21.jfif",
+title:"nike sneakers",
+price:350,
+
+},{ src:"/image-project/22.jfif",
+title:"black sunglasses",
+price:125,
+
+},{ src:"/image-project/23.jfif",
+title:"white dress",
+price:89,
+
+},{ src:"/image-project/24.jfif",
+title:"scarpain",
+price:900,
+
+},{ src:"/image-project/25.png",
+title:"red dress",
+price:50,
+
+},{ src:"/image-project/26.jfif",
+title:"Ray-ban sunglasses ",
+price:83,
+
+},{ src:"/image-project/27.jfif",
+title:"black shoes for men",
+price:70,
+
+},{ src:"/image-project/28.png",
+title:"brown sunglasses",
+price:110,
+
+},{ src:"/image-project/29.jfif",
+title:"bleu cap",
+price:15,
+
+},{ src:"/image-project/30.jfif",
+title:"T-shirt",
+price:21,
+
+}
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
